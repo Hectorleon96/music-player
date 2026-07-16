275f3fcdb9261cfa476eb7626549f04f37465e93
Hectorleon96 <jobleon.hector@gmail.com>
2026-07-16 06:53:33 -0600

    upload file, create audio instances, configure start and pause, render audio duration and current time

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,157 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+type PlayerState = "play" | "pause" | "stop";
+type Input = HTMLInputElement;
+type Button = HTMLButtonElement;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const inputFile = document.getElementById("input-file")! as Input;
+const playbackBtn = document.getElementById("playback-toggle")! as Button;
+const stopBtn = document.getElementById("control-stop")! as Button;
 
-<div class="ticks"></div>
+class Player {
+  state: PlayerState = "stop";
+  audioObjectUrl?: string;
+  playingSeconds: number = 0;
+  playingInterval: number | undefined = undefined;
+  currentAudioElement: HTMLAudioElement | null = null;
+  audioTrack: AudioTrack | null = null;
+  trackDuration: number = 0;
+  playerView: PlayerView;
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+  constructor(playerView: PlayerView) {
+    this.playerView = playerView;
+  }
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+  uploadFile(event: Event): void {
+    const { files } = event.target as Input;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+    if (files && files.length > 0) {
+      this.fileToAudio(files[0]);
+    } else {
+      this.resetFileInput();
+    }
+  }
+
+  fileToAudio(file: File) {
+    if (this.isValidFile(file) === false) {
+      this.resetFileInput();
+      return;
+    }
+
+    clearInterval(this.playingInterval);
+
+    this.audioObjectUrl = URL.createObjectURL(file);
+    const audioInstance = new Audio(this.audioObjectUrl);
+    this.currentAudioElement = audioInstance;
+
+    audioInstance.addEventListener("loadeddata", () => {
+      this.audioTrack = new AudioTrack(file, audioInstance);
+      this.trackDuration = Number(this.audioTrack.duration);
+      this.playerView.updateTrackDuration(this.audioTrack.duration);
+    });
+
+    this.playerView.updateTrackInfo(file);
+    playbackBtn.disabled = false;
+  }
+
+  isValidFile(file: File): Boolean {
+    const validOptions = ["mpeg", "mp3", "wav"];
+    return validOptions.some((option) => file.type.endsWith(option));
+  }
+
+  play() {
+    if (this.currentAudioElement === null) {
+      return;
+    }
+
+    this.state = "play";
+    this.playerView.updatePlaybackText("pause");
+    this.currentAudioElement?.play();
+
+    this.playingInterval = setInterval(() => {
+      this.playingSeconds++;
+
+      const minutes = Math.floor(this.trackDuration / 60);
+      const seconds = this.playingSeconds % 60;
+      const formattedTime = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+      this.playerView.updateTrackCurrentTime(formattedTime);
+    }, 1000);
+  }
+
+  pause() {
+    this.state = "pause";
+    this.playerView.updatePlaybackText("play");
+    this.currentAudioElement?.pause();
+    clearInterval(this.playingInterval);
+  }
+
+  playbackToggle() {
+    if (this.state !== "play") {
+      this.play();
+      return;
+    }
+
+    this.pause();
+  }
+
+  stop() {
+    if (this.currentAudioElement) {
+      this.currentAudioElement.load();
+    }
+
+    this.state = "stop";
+    this.playerView.updatePlaybackText("play");
+    this.playerView.updateTrackCurrentTime("0:00");
+    this.playingSeconds = 0;
+    clearInterval(this.playingInterval);
+  }
+
+  resetFileInput() {
+    inputFile.files = null;
+    inputFile.value = "";
+  }
+}
+
+class AudioTrack {
+  originalFile: File;
+  audio: HTMLAudioElement;
+  duration: string;
+
+  constructor(originalFile: File, audio: HTMLAudioElement) {
+    this.originalFile = originalFile;
+    this.audio = audio;
+    this.duration = (this.audio.duration / 60).toFixed(2);
+  }
+}
+
+class PlayerView {
+  trackNameElement: HTMLElement;
+  trackCurrentTime: HTMLElement;
+  trackDurationElement: HTMLElement;
+
+  constructor() {
+    this.trackNameElement = document.getElementById("track-name")!;
+    this.trackCurrentTime = document.getElementById("track-currenttime")!;
+    this.trackDurationElement = document.getElementById("track-duration")!;
+  }
+
+  updatePlaybackText(text: "play" | "pause") {
+    playbackBtn.textContent = text;
+  }
+
+  updateTrackInfo(file: File | null) {
+    this.trackNameElement.textContent = `Name: ${file?.name}`;
+  }
+
+  updateTrackDuration(duration: string) {
+    this.trackDurationElement.textContent = duration;
+  }
+
+  updateTrackCurrentTime(time: string) {
+    this.trackCurrentTime.textContent = time;
+  }
+}
+
+const player = new Player(new PlayerView());
+
+inputFile.addEventListener("change", (event) => player.uploadFile(event));
+playbackBtn.addEventListener("click", () => player.playbackToggle());
+stopBtn.addEventListener("click", () => player.stop());
