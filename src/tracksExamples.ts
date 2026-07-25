c61b20f706e0c1e73fc451dea3574d8b84b23816
Hectorleon96 <jobleon.hector@gmail.com>
2026-07-25 15:07:47 -0600

    fix example tracks src

--- src/tracksExamples.ts
+++ src/tracksExamples.ts
@@ -10,14 +10,14 @@ export const TRACKS_EXAMPLES: TrackExample[] = [
     id: 1,
     name: "Beats",
     artist: "Loksii",
-    fileSrc: "/tracks/beats-loksii.mp3",
+    fileSrc: `${import.meta.env.BASE_URL}tracks/beats-loksii.mp3`,
     // originalSrc: "https://pixabay.com/music/beats-no-copyright-music-211881/",
   },
   {
     id: 2,
     name: "Meditation/Spiritual",
     artist: "Alex Morgan",
-    fileSrc: "/tracks/meditation-spiritual-alex-morgan.mp3",
+    fileSrc: `${import.meta.env.BASE_URL}tracks/meditation-spiritual-alex-morgan.mp3`,
     // originalSrc:
     //   "https://pixabay.com/music/meditationspiritual-no-copyright-music-528321/",
   },
@@ -25,7 +25,7 @@ export const TRACKS_EXAMPLES: TrackExample[] = [
     id: 3,
     name: "Funk",
     artist: "Moodmode",
-    fileSrc: "/tracks/funk-moodmode.mp3",
+    fileSrc: `${import.meta.env.BASE_URL}tracks/funk-moodmode.mp3`,
     // originalSrc: "https://pixabay.com/music/funk-no-copyright-music-201745/",
   },
 ];
